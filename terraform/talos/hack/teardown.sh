770302d33b3f80b331a060ea11b142cb1c123bea
Tsvetoslav Dimov <baskski@gmail.com>
2025-11-25 16:30:53 +0000

    refactor: convert platform and media from appset to app of apps

--- terraform/talos/hack/teardown.sh
+++ terraform/talos/hack/teardown.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 kubens argocd
 argocd login argocd.cecobask.com --core
 argocd proj windows add default --kind=deny --schedule="* * * * *" --duration=30m --applications="*" --manual-sync
-argocd appset delete media --yes
-argocd appset delete platform --yes
+argocd app delete media --yes --wait
+argocd app delete platform --yes --wait
