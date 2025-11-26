f51ab11ad6cfef5985372b8e4fc5509514b65b12
Tsvetoslav Dimov <baskski@gmail.com>
2025-11-26 11:50:35 +0000

    refactor: exclude argocd from platform applicationset

--- terraform/talos/hack/teardown.sh
+++ terraform/talos/hack/teardown.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 kubens argocd
-argocd login argocd.cecobask.com --core
+argocd login --core
 argocd proj windows add default --kind=deny --schedule="* * * * *" --duration=30m --applications="*" --manual-sync
 argocd app delete media --yes --wait
 argocd app delete platform --yes --wait
