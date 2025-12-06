c0ec0a559d59879e8175649ca21a2f3a150dbf76
Tsvetoslav Dimov <baskski@gmail.com>
2025-12-06 18:14:12 +0000

    chore: improve talos cleanup script

--- terraform/talos/hack/cleanup.sh
+++ terraform/talos/hack/cleanup.sh
@@ -6,4 +6,6 @@ echo "Cleaning up applications..."
 kubens argocd
 argocd login --core
 argocd proj windows add default --kind=deny --schedule="* * * * *" --duration=30m --applications="*" --manual-sync
-argocd app delete media grafana prometheus --yes --wait
+argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox,csi-smb)'
+argocd app delete media --yes
+kubectl wait pv --all --for=delete --timeout=3m
