6526d1c9decef4b4f79d752a3b296b246f078aef
Tsvetoslav Dimov <baskski@gmail.com>
2025-12-07 20:01:22 +0000

    chore: reduce argocd sync window duration

--- terraform/talos/hack/cleanup.sh
+++ terraform/talos/hack/cleanup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Cleaning up applications..."
 kubens argocd
 argocd login --core
-argocd proj windows add default --kind=deny --schedule="* * * * *" --duration=30m --applications="*" --manual-sync
+argocd proj windows add default --kind=deny --schedule="* * * * *" --applications="*" --duration=3m
 argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox,csi-smb)'
 argocd app delete media --yes
 kubectl wait pv --all --for=delete --timeout=3m
