89420d3c375e4909e0aeda4634721452ded273be
Tsvetoslav Dimov <baskski@gmail.com>
2025-12-13 12:31:43 +0000

    feat: add longhorn storage

--- terraform/talos/hack/cleanup.sh
+++ terraform/talos/hack/cleanup.sh
@@ -6,6 +6,8 @@ echo "Cleaning up applications..."
 kubens argocd
 argocd login --core
 argocd proj windows add default --kind=deny --schedule="* * * * *" --applications="*" --duration=3m
-argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox,csi-smb)'
+argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox,csi-smb,longhorn)'
 argocd app delete media --yes
 kubectl wait pv --all --for=delete --timeout=3m
+kubectl patch setting deleting-confirmation-flag --namespace=longhorn-system --patch='{"value": "true"}' --type=merge
+kubectl wait job longhorn-uninstall --namespace=longhorn-system --for=condition=complete
