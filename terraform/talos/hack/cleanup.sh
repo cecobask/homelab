313b51a96d17067030f1944f7f7ffb30aa6dbe02
Tsvetoslav Dimov <baskski@gmail.com>
2025-12-22 19:11:17 +0200

    fix: exclude snapshot-controller from deletion

--- terraform/talos/hack/cleanup.sh
+++ terraform/talos/hack/cleanup.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-echo "Cleaning up applications..."
 kubens argocd
 argocd login --core
 argocd proj windows add default --kind=deny --schedule="* * * * *" --applications="*" --duration=3m
-argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox)'
+
+echo "Cleaning up applications..."
+argocd app delete --yes --selector='argocd.argoproj.io/instance, argocd.argoproj.io/instance notin (argocd,cilium,csi-proxmox,snapshot-controller)'
 argocd app delete media --yes
 kubectl wait pv --all --for=delete --timeout=3m
