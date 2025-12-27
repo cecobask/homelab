fda23a6ce09c0e5d7935763f50688f31052be6da
Tsvetoslav Dimov <baskski@gmail.com>
2025-12-27 17:11:25 +0200

    feat: automate argocd admin password update during bootstrap

--- terraform/talos/hack/bootstrap.sh
+++ terraform/talos/hack/bootstrap.sh
@@ -5,3 +5,10 @@ set -euo pipefail
 echo "Bootstrapping applications..."
 MANIFESTS_DIR="$(dirname "$0")/manifests"
 kubectl apply -k "$MANIFESTS_DIR"
+
+echo "Updating credentials..."
+kubens argocd
+TEMP_ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret --output=json | jq -r '.data.password' | base64 -d)
+argocd login argocd.cecobask.com --grpc-web --insecure --password="$TEMP_ARGOCD_ADMIN_PASSWORD" --username=admin
+argocd account update-password --account=admin --current-password="$TEMP_ARGOCD_ADMIN_PASSWORD" --grpc-web --new-password="$ARGOCD_ADMIN_PASSWORD"
+kubectl delete secret argocd-initial-admin-secret --ignore-not-found
