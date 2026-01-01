5312b9dc4703e746940eccd7d3a5a17f2568a8c4
Tsvetoslav Dimov <baskski@gmail.com>
2026-01-01 20:03:12 +0000

    refactor: simplify talos outputs

--- terraform/talos/outputs.tf
+++ terraform/talos/outputs.tf
@@ -7,8 +7,3 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
-
-output "secrets" {
-  value     = yamlencode(talos_machine_secrets.this.machine_secrets)
-  sensitive = true
-}
