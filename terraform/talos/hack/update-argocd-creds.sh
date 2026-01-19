42ec202a6aec9b71996c14b34ee27089750daa37
Tsvetoslav Dimov <baskski@gmail.com>
2026-01-19 21:08:39 +0000

    chore: remove homepage widgets

--- terraform/talos/hack/update-argocd-creds.sh
+++ terraform/talos/hack/update-argocd-creds.sh
@@ -8,5 +8,3 @@ TEMP_ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret --ou
 argocd login argocd.cecobask.com --grpc-web --insecure --password="$TEMP_ARGOCD_ADMIN_PASSWORD" --username=admin
 argocd account update-password --account=admin --current-password="$TEMP_ARGOCD_ADMIN_PASSWORD" --grpc-web --new-password="$ARGOCD_ADMIN_PASSWORD"
 kubectl delete secret argocd-initial-admin-secret --ignore-not-found
-READONLY_TOKEN=$(argocd account generate-token --account=readonly)
-kubectl create secret generic argocd-readonly --from-literal=token="$READONLY_TOKEN" --namespace=media
