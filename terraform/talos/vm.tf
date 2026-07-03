9f7c548d016b3a6ee8e8de6ee2d3d21c6a5c1c65
Tsvetoslav Dimov <baskski@gmail.com>
2026-07-03 22:11:45 +0100

    fix: proxmox vm disk drift because of iothread flapping between true and false

--- terraform/talos/vm.tf
+++ terraform/talos/vm.tf
@@ -8,12 +8,12 @@ resource "proxmox_virtual_environment_download_file" "this" {
 }
 
 resource "proxmox_virtual_environment_vm" "this" {
-  for_each      = var.vms
-  name          = each.key
-  node_name     = each.value.node_name
-  vm_id         = each.value.vm_id
-  machine       = "q35"
-  tags          = ["talos", each.value.machine_type]
+  for_each  = var.vms
+  name      = each.key
+  node_name = each.value.node_name
+  vm_id     = each.value.vm_id
+  machine   = "q35"
+  tags      = ["talos", each.value.machine_type]
   agent {
     enabled = true
   }
@@ -33,6 +33,7 @@ resource "proxmox_virtual_environment_vm" "this" {
     datastore_id = "local-lvm"
     file_id      = proxmox_virtual_environment_download_file.this[each.value.node_name].id
     interface    = "scsi0"
+    iothread     = true
     discard      = "on"
     size         = each.value.disk_gb
   }
