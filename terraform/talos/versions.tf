3aa87fa11f31d68f47c03d8de696b4d3d8ed2c8c
Tsvetoslav Dimov <baskski@gmail.com>
2026-07-04 12:45:39 +0100

    refactor: simplify flux bootstrap by letting its provider create namespace and secret

--- terraform/talos/versions.tf
+++ terraform/talos/versions.tf
@@ -15,10 +15,6 @@ terraform {
       source  = "integrations/github"
       version = "6.12.1"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "3.2.1"
-    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.84.1"
@@ -51,13 +47,6 @@ provider "flux" {
 }
 
 provider "github" {}
-
-provider "kubernetes" {
-  host                   = talos_cluster_kubeconfig.this.kubernetes_client_configuration.host
-  client_certificate     = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_certificate)
-  client_key             = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_key)
-  cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.ca_certificate)
-}
 
 provider "proxmox" {}
 
