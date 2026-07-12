57e243818c90a78bd41037f484cb21fc6fa9ad95
Tsvetoslav Dimov <baskski@gmail.com>
2026-07-12 20:10:15 +0100

    refactor: use core kustomization across all layers

--- terraform/talos/hack/cilium.sh
+++ terraform/talos/hack/cilium.sh
@@ -4,13 +4,13 @@ set -o pipefail
 set -u
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-HELM_RELEASE_FILE="$REPO_ROOT/kubernetes/infrastructure/controllers/cilium/helmrelease.yaml"
-HELM_REPO_FILE="$REPO_ROOT/kubernetes/infrastructure/controllers/cilium/source.yaml"
+HELM_RELEASE_FILE="$REPO_ROOT/kubernetes/infra/apps/cilium/helmrelease.yaml"
+HELM_REPO_FILE="$REPO_ROOT/kubernetes/infra/apps/cilium/source.yaml"
 HELM_CHART_URL="$(yq -r '.spec.url' "$HELM_REPO_FILE")"
 HELM_CHART_NAME="$(yq -r '.spec.chart.spec.chart' "$HELM_RELEASE_FILE")"
 HELM_CHART_VERSION="$(yq -r '.spec.chart.spec.version' "$HELM_RELEASE_FILE")"
 NAMESPACE="$(yq -r '.metadata.namespace' "$HELM_RELEASE_FILE")"
-NAMESPACE_FILE="$REPO_ROOT/kubernetes/infrastructure/controllers/cilium/namespace.yaml"
+NAMESPACE_FILE="$REPO_ROOT/kubernetes/infra/apps/cilium/namespace.yaml"
 OUT="$REPO_ROOT/terraform/talos/config/cilium.yaml"
 
 VALUES="$(mktemp)"
