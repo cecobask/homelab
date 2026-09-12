af7baf3e515708c87d0036d1760e1edd0215210c
Tsvetoslav Dimov <baskski@gmail.com>
2026-09-12 17:31:58 +0100

    chore: add cluster-destroy-delete label to all pvcs and set flux ks deletion policy to WaitForTermination

--- terraform/talos/hack/cleanup.sh
+++ terraform/talos/hack/cleanup.sh
@@ -8,4 +8,5 @@ flux delete kustomization services --silent
 flux delete kustomization media-apps --silent
 flux delete kustomization media-core --silent
 flux delete kustomization infra-apps --silent
+kubectl delete pvc --all-namespaces --ignore-not-found --selector=cluster-destroy-delete --wait
 flux delete kustomization infra-core --silent
